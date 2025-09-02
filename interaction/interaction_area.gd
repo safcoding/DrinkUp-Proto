e7e865f8102fce9cc73ea90a0f74762a2bfe764c
Safuan Hakim <155789635+safcoding@users.noreply.github.com>
2025-09-02 23:53:08 +0800

    stuff

--- interaction/interaction_area.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-class_name InteractionArea
-
-@export var action_name: String = "E"
-
-var interact: Callale = func():
-	pass
---
+++ scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+var speed = 100
+var  player_state
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	
+	if direction.x ==  0 and direction.y == 0:
+		player_state = "idle"
+	else:
+		player_state = "walking"
+		
+	velocity = direction * speed
+	move_and_slide()
+	
+	play_anim(direction)
+
+func play_anim(dir):
+	if player_state =="idle":
+		$AnimatedSprite2D.play("idle")
+	elif player_state == "walking":
+		if dir.y == -1:
+			$AnimatedSprite2D.play("walk_n")
+		if dir.y  == 1:
+			$AnimatedSprite2D.play("walk_s")
+		if dir.x == 1:
+			$AnimatedSprite2D.play("walk_e")
+		if dir.x == -1:
+			$AnimatedSprite2D.play("walk_w")
+			
+		if dir.x > 0.5 and dir.y  > 0.5:
+			$AnimatedSprite2D.play("walk_se")
+		if dir.x < -0.5 and dir.y  < -0.5:
+			$AnimatedSprite2D.play("walk_nw")
+		if dir.x > 0.5 and dir.y  < -0.5:
+			$AnimatedSprite2D.play("walk_ne")
+		if dir.x < -0.5 and dir.y  > 0.5:
+			$AnimatedSprite2D.play("walk_sw")
+
+func player():
+	pass
